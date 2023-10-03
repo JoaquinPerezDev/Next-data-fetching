790a3b89106f5625acb3e578e18d77ab07609d6d
LucasPerezDev@gmail.com <LucasPerezDev@gmail.com>
2023-10-03 11:57:09 -0400

    Integrated getStaticPaths for dynamic pages

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f27e6eb61637a12a"; }
+/******/ 		__webpack_require__.h = function() { return "738e7cfb7a18cbc4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
