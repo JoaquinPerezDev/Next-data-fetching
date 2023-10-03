8ca893e558fd375226e17c7479249749530ee92b
LucasPerezDev@gmail.com <LucasPerezDev@gmail.com>
2023-10-03 12:35:25 -0400

    Added server-side rendering w/ getServerSideProps

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "20d4fa8ebc7ff30e"; }
+/******/ 		__webpack_require__.h = function() { return "0ef8ff1da1488958"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
